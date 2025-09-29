06a6515323837a8456035b35a1c6b7dcf3f22890
alexnrn2204 <alexnrn2204@gmail.com>
2025-09-29 23:27:56 +0300

    Solution_v3

--- app/main.py
+++ app/main.py
@@ -10,14 +10,20 @@ class Animal:
             health: int = 100,
             hidden: bool = False) -> None:
         self.name = name
-        self.health = health
         self.hidden = hidden
-        Animal.alive.append(self)
+        self.health = health
+        self.on_set_health()
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
+
+    def on_set_health(self) -> None:
+        if self.health > 0 and self not in Animal.alive:
+            Animal.alive.append(self)
+        elif self.health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -33,5 +39,4 @@ class Carnivore(Animal):
             return
 
         beast.health -= 50
-        if beast.health <= 0:
-            Animal.alive.remove(beast)
+        beast.on_set_health()
